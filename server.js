d7ce1d13dc913109f2927d3c0eab42d2725182b4
Brian Albert <brian.david.albert@gmail.com>
2019-03-28 15:02:17 -0400

    Fixed runServer and closeServer functions

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 
 const app = express();
+app.use(express.json());
 
 const blogPostRouter = require('./blogPostRouter');
 
@@ -18,6 +19,43 @@ app.get('/', (req, res) => {
 app.use('/blog-posts', blogPostRouter);
 app.use('/blog-posts/:id', blogPostRouter);
 
-app.listen(process.env.PORT || 8080, () => {
-    console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
-  });  
+
+let server; 
+
+// start server and return Promise
+function runserver() {
+  const port = process.env.PORT || 8080;
+  return new Promise((resolve, reject) => {
+    server = app
+      .listen(port, () => {
+        console.log(`Your app is listening on port ${port}`);
+        resolve(server);
+      })
+      .on("error", err => {
+        reject(err);
+      });
+  });
+}
+
+function closeServer() {
+  return new Promise((resolve, reject) => {
+    console.log("Closing server");
+    server.close(err => {
+      if (err) {
+        reject(err);
+        return;
+      }
+      resolve();
+    });
+  });
+}
+
+if (require.main === module) {
+  runserver().catch(err => console.error(err));
+}
+
+module.exports = { app, runServer, closeServer };
+
+ 
+
+
